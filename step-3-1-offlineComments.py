1d3fac7235f205e4471cc61ff7b125f86b76c816
Zijian <arrowzeke@gmail.com>
2019-02-09 12:24:04 -0500

    [step-3-1] included more doctor info columns

--- step-3-1-offlineComments.py
+++ step-3-1-offlineComments.py
@@ -12,8 +12,6 @@ wait = 1
 defaultHeaders = {'User-Agent': ''}
 defaultSeperator = ','
 
-# sourceFilePath = 'D:/haodfdata/step-2-result.csv'
-# resultFilePath = 'D:/haodfdata/step-3-1-result.csv'
 sourceFilePath = './step-2-result.csv'
 resultFilePath = './step-3-1-result.csv'
 
@@ -31,22 +29,18 @@ with open(sourceFilePath) as sf:
 
     with open(resultFilePath, 'w') as rf:
 
-        resultHeaders = [ '医生姓名','医生ID','患者','时间','所患疾病',
+        resultHeaders = [ '医生姓名','医院科室','医生ID','患者','时间','所患疾病',
                           '看病目的','治疗方式','疗效','态度',
                           '选择该医生就诊的理由','本次挂号途径','目前病情状态',
-                          '本次看病费用总计','分享','该患者的其他分享' ]
+                          '本次看病费用总计','分享','该患者的其他分享',
+                          '信息中心页','地区' ]
 
         rf.write(defaultSeperator.join(resultHeaders))
 
         for row in sf:
 
-            # REMOVE ME
-            # if counter%100 != 0:
-            #     counter += 1
-            #     continue
-            # END
-
-            [n,i,p,l] = row.strip().split(defaultSeperator)
+            # [n,i,p,l,dp,il,rg] ~= [医生姓名,医生ID,个人网站,评价分享链接,医院科室,信息中心页,地区]
+            [n,i,p,l,dp,il,rg] = row.strip().split(defaultSeperator)
             counter+=1
             print('No '+str(counter)+', Crawling offline comments of '+n+', '+l)
             if l=='':
@@ -112,11 +106,11 @@ with open(sourceFilePath) as sf:
 
                         comment = patientComment.text.strip().replace('\n',' ')
 
-                        rf.write('\n'+defaultSeperator.join([n,i,infoMap['患者'],infoMap['时间'],infoMap['所患疾病'],
+                        rf.write('\n'+defaultSeperator.join([n,dp,i,infoMap['患者'],infoMap['时间'],infoMap['所患疾病'],
                                                              infoMap['看病目的'],infoMap['治疗方式'],infoMap['疗效'],
                                                              infoMap['态度'],infoMap['选择该医生就诊的理由'],
                                                              infoMap['本次挂号途径'],infoMap['目前病情状态'],
-                                                             infoMap['本次看病费用总计'],comment,infoMap['该患者的其他分享']]))
+                                                             infoMap['本次看病费用总计'],comment,infoMap['该患者的其他分享'],il,rg]))
 
                 except Exception as e:
                     traceback.print_exc()
